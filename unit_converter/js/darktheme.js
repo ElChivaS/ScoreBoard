ac2bde7651cc321377042313018e4b8347408494
Hermann Caraballo <hermann.caraballo@gmail.com>
2022-09-07 21:58:29 -0300

    Separate DarkTheme function to a new file.

--- unit_converter/js/darktheme.js
+++ unit_converter/js/darktheme.js
@@ -1,47 +1,10 @@
-/*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
-*/
-import { themeToggle } from './darktheme.js';
-
-const userUnitEL = document.getElementById('user-unit');
-const convertBtn = document.getElementById('convert-btn');
-const themeBtn = document.getElementById('theme-btn');
-// const sectCont = document.getElementById('sect-cont');
-// const h3Length = document.getElementById('h3-length');
-// const h3Volume = document.getElementById('h3-volume');
-// const h3Mass = document.getElementById('h3-mass');
-// const itemLength = document.getElementById('item-length');
-// const itemVolume = document.getElementById('item-volume');
-// const itemMass = document.getElementById('item-mass');
-// const logoTxt = document.getElementById('logo-txt');
-
-let resultLength = document.getElementById("result-length")
-let resultVolume = document.getElementById("result-volume")
-let resultMass = document.getElementById("result-mass")
-
-convertBtn.addEventListener("click", function convertThisShit() {
-    let pound = (userUnitEL.value * 2.204).toFixed(2);
-    let kilo = (userUnitEL.value / 2.204).toFixed(2);
-    let gallon = (userUnitEL.value * 2.204).toFixed(2);
-    let liter = (userUnitEL.value / 2.204).toFixed(2);
-    let feet = (userUnitEL.value * 3.281).toFixed(2);
-    let meter = (userUnitEL.value / 3.281).toFixed(2);
-    
-    resultLength.innerHTML = `${userUnitEL.value} Meters = ${feet} Feet | ${userUnitEL.value} Feet = ${meter} Meters`
-    resultVolume.innerHTML = `${userUnitEL.value} Liters = ${gallon} Gallons | ${userUnitEL.value} Gallons = ${liter} Liters`
-    resultMass.innerHTML = `${userUnitEL.value} Kilos = ${pound} Pounds | ${userUnitEL.value} Punds = ${kilo} Kilos`
-})
-
-themeBtn.addEventListener('click', themeToggle());
+export function themeToggle() {
 
 
-// const convertThisShit = (num) => {}
 
-/* Toggle Dark || Light Mode *
-themeBtn.addEventListener('click', function() {
-    console.log(themeBtn.textContent)
+/* Toggle Dark || Light Mode */
+// function themeToggle() {
+    // console.log(themeBtn.textContent)
     if(themeBtn.textContent === 'Dark Theme') {
         themeBtn.textContent = "Light Theme"
         sectCont.classList.remove('light-bg')
@@ -97,5 +60,17 @@ themeBtn.addEventListener('click', function() {
         logoTxt.classList.remove('logo-dark')
         logoTxt.classList.add('logo-light')
     }
-})
-*/
+}
+const themeBtn = document.getElementById('theme-btn');
+const sectCont = document.getElementById('sect-cont');
+const h3Length = document.getElementById('h3-length');
+const h3Volume = document.getElementById('h3-volume');
+const h3Mass = document.getElementById('h3-mass');
+const itemLength = document.getElementById('item-length');
+const itemVolume = document.getElementById('item-volume');
+const itemMass = document.getElementById('item-mass');
+const logoTxt = document.getElementById('logo-txt');
+
+let resultLength = document.getElementById("result-length")
+let resultVolume = document.getElementById("result-volume")
+let resultMass = document.getElementById("result-mass")

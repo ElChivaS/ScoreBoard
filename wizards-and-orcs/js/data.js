117ce4350a66819ac1df88087235c6287864f919
Hermann Caraballo <hermann.caraballo@gmail.com>
2022-09-15 01:02:20 -0400

    Added monsters objects.

--- wizards-and-orcs/js/data.js
+++ wizards-and-orcs/js/data.js
@@ -1,15 +1,33 @@
 const characterData = {
     hero: {
-        name: "Wizard", 
+        name: "Wizard",
         avatar: "wizard.png",
-        health: "60",
-        diceCount: 3
+        health: "100",
+        diceCount: 3,
+        currentDiceScore: []
     },    
     monster: {
-        name: "Orc", 
+        name: "Orc",
         avatar: "orc.png",
-        health: "10",
-        diceCount: 1
+        health: "100",
+        diceCount: 1,
+        currentDiceScore: []
+    },
+    demon: {
+        name: "Demon",
+        avatar: "demon.png",
+        health: "25",
+        diceCount: 2,
+        currentDiceScore: []
+        
+    },
+    goblin: {
+        name: "Goblin",
+        avatar: "goblin.png",
+        health: "20",
+        diceCount: 3,
+        currentDiceScore: []
+
     }
 }
 

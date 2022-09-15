5c220d4345d9c43648771879d468833d38f738a4
Hermann Caraballo <hermann.caraballo@gmail.com>
2022-09-15 01:03:32 -0400

    Added Chars and its render.

--- wizards-and-orcs/js/wao.js
+++ wizards-and-orcs/js/wao.js
@@ -1,11 +1,63 @@
 import characterData from './data.js'
 import Character from './Character.js'
 
-const wizard = new Character(characterData.hero)
-const orc = new Character(characterData.monster)
+let monstersArray = ["orc", "demon", "goblin"]
+let isWaiting = false
+
+function getNewMonster() {
+    const nextMonsterData = characterData[ monstersArray.shift() ]
+    return nextMonsterData ? new Character(nextMonsterData) : {}
+}
+
+function attack() {
+    if (!isWaiting) {
+        wizard.setDiceHtml()
+        monster.setDiceHtml()
+        wizard.takeDamage(monster.currentDiceScore)
+        monster.takeDamage(wizard.currentDiceScore)
+        render()
+        
+        if (wizard.isDead) {
+            endGame()
+        } else if (monster.isDead) {
+            isWaiting = true
+            if (monstersArray.length > 0) {
+                setTimeout(() => {
+                    monster = getNewMonster()
+                    render()
+                    isWaiting = false
+                }, 3000)
+            }
+        } else {
+            endGame()
+        }
+    }
+}
+
+function endGame() {
+    isWaiting = true
+    const endMessage =  wizard.health === 0 && monster.health === 0 ? "No victors - all creatures are dead" : 
+                        wizard.health > 0 ? "The Wizard Wins" : `The monsters are Victorious`
+
+    const endEmoji = wizard.health > 0 ? "🔮" : "☠️"
+    setTimeout(() => {
+        document.body.innerHTML =  `<div class="end-game">
+                                        <h2>Game Over</h2>
+                                        <h3>${endMessage}</h3>
+                                        <p class="end-emoji">${endEmoji}</p>
+                                    </div>`
+    }, 3000)
+}
+
+document.getElementById('attack-button').addEventListener('click', attack)
 
 function render() {
     document.getElementById('hero').innerHTML = wizard.getCharacterHtml()
-    document.getElementById('monster').innerHTML = orc.getCharacterHtml()
+    document.getElementById('monster').innerHTML = monster.getCharacterHtml()
 }
+
+const wizard = new Character(characterData.hero)
+// const orc = new Character(characterData.monster) // const orc IS NOW let monster
+let monster = getNewMonster()
+
 render()

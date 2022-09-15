aac8fbf0fed9abdf7053a733666ecf3095330ac4
Hermann Caraballo <hermann.caraballo@gmail.com>
2022-09-15 01:01:45 -0400

    Fixed constructor to set chars more efficiently.

--- wizards-and-orcs/js/Character.js
+++ wizards-and-orcs/js/Character.js
@@ -1,25 +1,50 @@
-import { getDiceRollArray } from './utils.js'
+import { getDiceRollArray, getDicePlaceholderHtml, getPercentage } from './utils.js'
 
-export default function Character(data) {
+function Character(data) {
     Object.assign(this, data)
+
+    this.maxHealth = this.health
+
+    this.diceHtml = getDicePlaceholderHtml(this.diceCount)
     
-    this.getDiceHtml = function(diceCount) {
-        return getDiceRollArray(diceCount).map(function(num) {
-            return `<div class="dice">${num}</div>`
-        }).join("")
+    this.takeDamage = function(attackScoreArray) {
+        const totalAttackScore = attackScoreArray.reduce( (total, num) => total + num )
+        this.health -= totalAttackScore
+        if (this.health <= 0) {
+            this.isDead = true
+            this.health = 0
+            // disable attack button
+            // create newGame button
+        }
+    }
+    
+    this.setDiceHtml = () => {
+        this.currentDiceScore = getDiceRollArray(this.diceCount)
+        this.diceHtml = this.currentDiceScore.map( num => `<div class="dice">${num}</div>` ).join('')
+    }
+
+    this.getHealthBarHtml = function() {
+        const percent = getPercentage(this.health, this.maxHealth)        
+        return `
+                <div class="health-bar-outer">
+                    <div class="health-bar-inner ${percent < 26 ? "danger" : ""}" style="width: ${percent}%"></div>
+                </div>`
     }
 
     this.getCharacterHtml = function () {
-        const { elementID, name, avatar, health, diceCount } = this
-        const diceHtml = this.getDiceHtml(diceCount) // ADD this TO THE METHOD CODE
+        const { elementID, name, avatar, health, diceCount, diceHtml, totalAttackScore } = this
+        const healthBar = this.getHealthBarHtml()
         
         return `<div class='character-card'>
                     <h4 class="name">${name}</h4>
                     <img class='avatar' src='./assets/img/${avatar}' alt="a wizard on attack position" />
                     <p class="health">Health: <b>${health}</b> </p>
+                    ${healthBar}
                     <div class="dice-container">
                         ${diceHtml}
                     </div>
                 </div>`
     }
 }
+
+export default Character
